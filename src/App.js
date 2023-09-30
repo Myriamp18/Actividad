ec270927bb269b5ee6c6fb0b6e3de41a13922449
Myriam Pedroza <11456@utl.edu.mx>
2023-09-30 16:07:00 -0600

    agrege api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import MyProvider from './app/MyStateProvider';
+import ShowState from './components/Showstate';
+import LogIn from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+
+   <div> 
+    <h2>LOGIN</h2>
+    <MyProvider>
+      <LogIn />
+      <ShowState />
+    </MyProvider>
     </div>
   );
-}
 
 export default App;
